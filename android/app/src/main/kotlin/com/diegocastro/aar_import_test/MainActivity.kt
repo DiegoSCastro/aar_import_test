635fda56305dd1f10c3dc92813090e38b427e892
Diego Castro <omegadiegocastro@gmail.com>
2024-07-15 22:26:07 -0300

    feat(native): add platform channel

--- android/app/src/main/kotlin/com/diegocastro/aar_import_test/MainActivity.kt
+++ android/app/src/main/kotlin/com/diegocastro/aar_import_test/MainActivity.kt
@@ -3,37 +3,56 @@ package com.diegocastro.aar_import_test
 import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
+import com.mercadolibre.android.point_integration_sdk.nativesdk.MPManager
 import com.mercadolibre.android.point_integration_sdk.nativesdk.configurable.MPConfigBuilder
 
 class MainActivity: FlutterActivity() {
+
     private val CHANNEL = "com.example.yourapp/native"
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
-                call, result ->
-            if (call.method == "getConfig") {
-                val config = getConfig()
-                if (config != null) {
-                    result.success(config)
-                } else {
-                    result.error("UNAVAILABLE", "Config not available.", null)
+            call, result ->
+            when (call.method) {
+                "getConfig" -> {
+                    val success = getConfig()
+                    if (success) {
+                        result.success("Config initialized successfully")
+                    } else {
+                        result.error("UNAVAILABLE", "Config initialization failed.", null)
+                    }
                 }
-            } else {
-                result.notImplemented()
+                "getPlatformVersion" -> {
+                    result.success("Android ${android.os.Build.VERSION.RELEASE}")
+                }
+                else -> {
+                    result.notImplemented()
+                }
             }
         }
     }
 
-    private fun getConfig(): String {
-        val application = applicationContext as android.app.Application
-        val configBuilder = MPConfigBuilder(application, "193213529837179")
-        val config = configBuilder
-            .withBitmapPrinterConfig()
-            .withBluetoothConfig()
-            .withCameraScanner()
-            .build()
-        return config.toString()
+    companion object {
+        private const val DEMO_APP_CLIENT_ID = "193213529837179"
+    }
+
+    private fun getConfig(): Boolean {
+        return try {
+            val application = applicationContext as android.app.Application
+            val configBuilder = MPConfigBuilder(application, DEMO_APP_CLIENT_ID)
+            val config = configBuilder
+                .withBitmapPrinterConfig()
+                .withBluetoothConfig()
+                .withCameraScanner()
+                .build()
+            
+            // MPManager.initialize(this, config)
+            true
+        } catch (e: Exception) {
+            false
+        }
     }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -15,20 +17,37 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Flutter-Native Communication'),
+          title: const Text('Flutter-Native Communication'),
         ),
         body: Center(
-          child: ElevatedButton(
-            onPressed:
-                () async {
-              try {
-                final String result = await platform.invokeMethod('getConfig');
-                print(result);
-              } on PlatformException catch (e) {
-                print("Failed to get config: '${e.message}'.");
-              }
-            },
-            child: Text('Get Config from Native'),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton(
+                onPressed: () async {
+                  try {
+                    final String result =
+                        await platform.invokeMethod('getConfig');
+                    print(result);
+                  } on PlatformException catch (e) {
+                    print("Failed to get config: '${e.message}'.");
+                  }
+                },
+                child: const Text('Get Config from Native'),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  try {
+                    final String result =
+                        await platform.invokeMethod('getPlatformVersion');
+                    print(result);
+                  } on PlatformException catch (e) {
+                    print("Failed to get config: '${e.message}'.");
+                  }
+                },
+                child: const Text('get Platform Version'),
+              ),
+            ],
           ),
         ),
       ),
